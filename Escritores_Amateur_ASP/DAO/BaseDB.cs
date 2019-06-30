9e48f63d87728a39c0d6df60b7de893ff5e130f1
Ricardo Canul <ricardocanul7@gmail.com>
2019-06-30 13:37:10 -0500

    Se crea la clase BaseDB que funciona como conexion a la base de datos

---
+++ Escritores_Amateur_ASP/DAO/BaseDB.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Escritores_Amateur_ASP.DAO
+{
+    public class BaseDB
+    {
+        private SqlConnection cnn;
+        private SqlDataReader dr;
+        private string stringConexion;
+        private SqlCommand cmd = new SqlCommand();
+        private SqlDataAdapter da;
+
+        public SqlConnection Cnn { get => cnn; set => cnn = value; }
+        public SqlDataReader Dr { get => dr; set => dr = value; }
+        public SqlCommand Cmd { get => cmd; set => cmd = value; }
+        public SqlDataAdapter Da { get => da; set => da = value; }
+
+        public BaseDB()
+        {
+            establecerConexion();
+        }
+
+        private void establecerConexion()
+        {
+            stringConexion = "Data source = DESKTOP-PC\\SQLEXPRESS; initial catalog = db_escritores_amateur; integrated security=true;";
+            Cnn = new SqlConnection(stringConexion);
+        }
+
+        public void abrirConexion()
+        {
+            Cnn.Open();
+        }
+
+        public void cerrarConexion()
+        {
+            Cnn.Close();
+        }
+
+        public SqlDataAdapter execQuery(string query)
+        {
+            Da = new SqlDataAdapter();
+            if(Cnn.State != System.Data.ConnectionState.Open)
+            {
+                abrirConexion();
+                Cmd = new SqlCommand(query, Cnn);
+                Dr = Cmd.ExecuteReader();
+                Cmd.Connection = Cnn;
+
+                Cmd.CommandText = query;
+                Da.SelectCommand = cmd;
+                cerrarConexion();
+                return Da;
+            }
+            else
+            {
+                cerrarConexion();
+                Da = null;
+                return Da;
+            }
+        }
+
+        public int execNonQuery(string query)
+        {
+            Da = new SqlDataAdapter();
+            if(Cnn.State == System.Data.ConnectionState.Open)
+            {
+                abrirConexion();
+                Cmd = new SqlCommand(query, Cnn);
+                Dr = Cmd.ExecuteReader();
+                Cmd.Connection = Cnn;
+                Cmd.CommandText = query;
+                int i = cmd.ExecuteNonQuery();
+                if (i == 0)
+                    return 0;
+                else
+                    return 1;
+            }
+            else
+            {
+                cerrarConexion();
+                return 1;
+            }
+        }
+    }
+}

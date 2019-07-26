a4c7156c5e4893d2032a714523c7365af36968e7
Ricardo Canul <ricardocanul7@gmail.com>
2019-07-26 11:18:03 -0500

    Se cambia inputs de registro para poder recuperar datos al registrar un usuario pero el metodo de regitrar usuario aun no funciona

--- Escritores_Amateur_ASP/DAO/Usuario.cs
+++ Escritores_Amateur_ASP/DAO/Usuario.cs
@@ -55,5 +55,48 @@ namespace Escritores_Amateur_ASP.DAO
             bd.execQuery(sql).Fill(dt);
             return dt;
         }
+
+        // NO FUNCIONA AUN
+        public int creaUsuario(object obj)
+        {
+            BO.Usuario data = (BO.Usuario)obj;
+            bd = new BaseDB();
+            bd.Cmd.CommandType = CommandType.StoredProcedure;
+
+            sql = "EXEC SP_REGISTRO_USUARIO @nombre, @apellido_pat, @apellido_mat, @correo, @avatar, @municipio, @telefono, @sitio_web, @biografia, @username, @contrasenia, @tipo_usuario";
+
+            bd.Cmd.Parameters.Add("@nombre", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@apellido_pat", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@apellido_mat", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@correo", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@avatar", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@municipio", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@telefono", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@sitio_web", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@biografia", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@username", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@contrasenia", SqlDbType.VarChar);
+            bd.Cmd.Parameters.Add("@tipo_usuario", SqlDbType.VarChar);
+
+            bd.Cmd.Parameters["@nombre"].Value = data.Nombre;
+            bd.Cmd.Parameters["@apellido_pat"].Value = data.Apellido_pat;
+            bd.Cmd.Parameters["@apellido_mat"].Value = data.Apellido_mat;
+            bd.Cmd.Parameters["@correo"].Value = data.Correo;
+            bd.Cmd.Parameters["@avatar"].Value = data.Avatar;
+            bd.Cmd.Parameters["@municipio"].Value = data.Municipio;
+            bd.Cmd.Parameters["@telefono"].Value = data.Telefono;
+            bd.Cmd.Parameters["@sitio_web"].Value = data.Sitio_web;
+            bd.Cmd.Parameters["@biografia"].Value = data.Biografia;
+            bd.Cmd.Parameters["@username"].Value = data.Username;
+            bd.Cmd.Parameters["@contrasenia"].Value = data.Contrasenia;
+            bd.Cmd.Parameters["@tipo_usuario"].Value = data.Tipo_usuario;
+
+            int i = bd.execNonQuery(sql);
+            if (i == 0)
+            {
+                return 0;
+            }
+            return 1;
+        }
     }
 }

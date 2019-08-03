8ef94031640e1875165f73181cdbc9b20fa7df0f
Alan Moreno <alanalexismartinez@gmail.com>
2019-08-03 00:06:04 -0500

    Agregué wf para un nuevo capitulo

---
+++ Escritores_Amateur_ASP/GUI/wfNuevoCapitulo.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage/mpPrincipal.Master" AutoEventWireup="true" CodeBehind="wfNuevoCapitulo.aspx.cs" Inherits="Escritores_Amateur_ASP.GUI.wfNuevoCapitulo" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cphTitulo" runat="server">
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphContenido" runat="server">
+    <div class="row" style="margin-top: 110px; margin-bottom: 80px;">
+        <div class="col" style="width: 100%">
+
+           <article class="card-body">
+                    <form>
+                        <div class="alert alert-info card-header">
+                            <h2>
+                                <asp:Label ID="lblCAPITULO" runat="server">  Nuevo capítulo </asp:Label>
+                            </h2>
+                        </div>
+                        <div class="form-row">
+                            <div class="col form-group">
+                                <label>Nombre del capítulo</label>
+                                <asp:TextBox ID="input_nombre" runat="server" CssClass="form-control"></asp:TextBox>
+                                <label>Contenido</label>
+                                <asp:TextBox ID="input_contenido" runat="server" CssClass="form-control"></asp:TextBox>
+                            </div>
+                        </div>
+                    </form>
+                </article>
+
+        </div>
+    </div>
+</asp:Content>
---
+++ Escritores_Amateur_ASP/GUI/wfNuevoCapitulo.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Escritores_Amateur_ASP.GUI {
+    
+    
+    public partial class wfNuevoCapitulo {
+        
+        /// <summary>
+        /// Control lblCAPITULO.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCAPITULO;
+        
+        /// <summary>
+        /// Control input_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox input_nombre;
+        
+        /// <summary>
+        /// Control input_contenido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox input_contenido;
+    }
+}

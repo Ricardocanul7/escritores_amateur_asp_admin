c64950071e28109aa941f6de2c01bd0bbb602833
Alan Moreno <alanalexismartinez@gmail.com>
2019-08-03 00:05:27 -0500

    agregué wf para visualizar los cap ya existentes y agregar nuevos cap

---
+++ Escritores_Amateur_ASP/GUI/wfAgregarCapitulo.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage/mpPrincipal.Master" AutoEventWireup="true" CodeBehind="wfAgregarCapitulo.aspx.cs" Inherits="Escritores_Amateur_ASP.GUI.wfAgregarCapitulo" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cphTitulo" runat="server">
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphContenido" runat="server">
+    <div class="row" style="margin-top: 110px; margin-bottom: 80px;">
+        <div class="col" style="width: 100%">
+            <div class="card">
+                <h5 class="card-header">Capitulos
+                    <asp:Button ID="btnAgregarCap" runat="server" Text="+ Agregar capitulo" OnClick="btnAgregarCap_Click" CssClass="float-right btn btn-outline-primary mt-1" />
+                </h5>
+                <div class="card-body">
+                    <asp:Image ID="imgPortada" CssClass="rounded float-left" runat="server" />
+                    <h5 class="card-title">
+                        <asp:Label ID="lblTitulo" runat="server" Text="¿Por qué a mi?"></asp:Label>
+                    </h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.
+                        <asp:Button ID="btnEditar" runat="server" Text="Editar" CssClass="float-right btn btn-outline-primary mt-1" />
+                    </p>
+                 </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ Escritores_Amateur_ASP/GUI/wfAgregarCapitulo.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Escritores_Amateur_ASP.GUI {
+    
+    
+    public partial class wfAgregarCapitulo {
+        
+        /// <summary>
+        /// Control btnAgregarCap.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarCap;
+        
+        /// <summary>
+        /// Control imgPortada.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgPortada;
+        
+        /// <summary>
+        /// Control lblTitulo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTitulo;
+        
+        /// <summary>
+        /// Control btnEditar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEditar;
+    }
+}

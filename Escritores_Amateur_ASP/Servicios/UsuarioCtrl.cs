94d9b1f28e168b7ef998f1228cc954ba6363f5ea
Alan Moreno <alanalexismartinez@gmail.com>
2019-08-04 14:57:06 -0500

    Agregué métodos para verificar las acciones crear, actualizar, eliminar

--- Escritores_Amateur_ASP/Servicios/UsuarioCtrl.cs
+++ Escritores_Amateur_ASP/Servicios/UsuarioCtrl.cs
@@ -21,5 +21,32 @@ namespace Escritores_Amateur_ASP.Servicios
             dt = objDAO.devuelveDatos(obj);
             return dt;
         }
+        public string creaUsuario(object obj)
+        {
+            int i = objDAO.creaUsuario(obj);
+            if (i == 1)
+            {
+                return "La operación se realizó de manera correcta";
+            }
+            return "La operación no pudo realizarce con éxito";
+        }
+        public string actualizaObj(object obj)
+        {
+            int i = objDAO.actualizaUsuario(obj);
+            if (i == 1)
+            {
+                return "La operación se realizó de manera correcta";
+            }
+            return "La operación no pudo realizarce con éxito";
+        }
+        public string eliminaObj(object obj)
+        {
+            int i = objDAO.eliminaDatos(obj);
+            if (i == 1)
+            {
+                return "La operación se realizó de manera correcta";
+            }
+            return "La operación no pudo realizarce con éxito";
+        }
     }
 }

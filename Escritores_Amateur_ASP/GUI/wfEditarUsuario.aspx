be617ff13a30879c6d529058e4201c397f523bea
Alan Moreno <alanalexismartinez@gmail.com>
2019-08-04 14:52:39 -0500

    Cree form para editar usuario junto con los campos

---
+++ Escritores_Amateur_ASP/GUI/wfEditarUsuario.aspx
@@ -0,0 +1,99 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage/mpAdmin.Master" AutoEventWireup="true" CodeBehind="wfEditarUsuario.aspx.cs" Inherits="Escritores_Amateur_ASP.GUI.wfEditarUsuario" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cphTitulo" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphContenido" runat="server">
+    <div class="card">
+        <div class="card-body">
+
+            <div class="row">       
+                <div class="col-md-6" > <!--NOMBRE-->
+                    <div class="form-group">
+                        <asp:Label ID="lblNombre" runat="server" Text="Nombre:"></asp:Label>
+                        <asp:TextBox ID="txtNombre" class="form-control" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="col-md-6" >
+                    <div class="form-group"> <!--Avatar-->
+                         <asp:Label ID="lblAvatar" runat="server" Text="Avatar:"></asp:Label>
+                         <asp:TextBox ID="txtAvatar" class="form-control" runat="server"></asp:TextBox>                    
+                    </div>
+                </div>
+            </div>
+            <div class="row">       
+                <div class="col-md-6" > <!--Apellido Paterno-->
+                    <div class="form-group">
+                         <asp:Label ID="lblApellidoPat" runat="server" Text="Apellido Paterno:"></asp:Label>
+                         <asp:TextBox ID="txtApellidoPat" class="form-control" runat="server"></asp:TextBox>                    
+                    </div>
+                </div> 
+                <div class="col-md-6" >
+                    <div class="form-group"> <!--Apellido Materno-->
+                         <asp:Label ID="lblApellidoMat" runat="server" Text="Apellido Materno:"></asp:Label>
+                         <asp:TextBox ID="txtApellidoMat" class="form-control" runat="server"></asp:TextBox>                    
+                    </div>
+                </div> 
+            </div>
+            <div class="row">       
+                <div class="col-md-12" > <!--Correo-->
+                    <div class="form-group">
+                         <asp:Label ID="lblCorreo" runat="server" Text="Correo:"></asp:Label>
+                         <asp:TextBox ID="txtCorreo" class="form-control" runat="server"></asp:TextBox>                    
+                    </div>
+                </div>
+            </div>
+            <div class="row">       
+                <div class="col-md-6" > <!--Telefono-->
+                    <div class="form-group">
+                         <asp:Label ID="lblTelefono" runat="server" Text="Teléfono:"></asp:Label>
+                         <asp:TextBox ID="txtTelefono" class="form-control" runat="server"></asp:TextBox>                    
+                    </div>
+                </div>
+                 <div class="col-md-6" >
+                    <div class="form-group"> <!--municipio-->
+                         <asp:Label ID="lblMunicipio" runat="server" Text="Municipio:"></asp:Label>
+                         <asp:TextBox ID="txtMunicipio" class="form-control" runat="server"></asp:TextBox>                    
+                    </div>
+                </div>  
+            </div>
+            <div class="row">       
+                <div class="col-md-12" >
+                    <div class="form-group"> <!--Sitio web-->
+                         <asp:Label ID="lblSitioWeb" runat="server" Text="Sitio web:"></asp:Label>
+                         <asp:TextBox ID="txtSitioWeb" class="form-control" runat="server"></asp:TextBox>                    
+                    </div>
+                </div>
+            </div>
+            <div class="row">       
+                <div class="col-md-6" >
+                    <div class="form-group"> <!--Username-->
+                         <asp:Label ID="lblUsername" runat="server" Text="Username:"></asp:Label>
+                         <asp:TextBox ID="txtUsername" class="form-control" runat="server"></asp:TextBox>                    
+                    </div>
+                </div> 
+                <div class="col-md-6" >
+                    <div class="form-group"> <!--tipo_usuario-->
+                         <asp:Label ID="lblTipoUser" runat="server" Text="Tipo de usuario:"></asp:Label>
+                         <asp:DropDownList ID="drpTipoUser" runat="server"></asp:DropDownList>
+                    </div>
+                </div>
+            </div>
+            <div class="row">       
+                <div class="col-md-6" > <!--Contraseña-->
+                    <div class="form-group">
+                         <asp:Label ID="lblPassword" runat="server" Text="Contraseña:"></asp:Label>
+                         <asp:TextBox ID="txtPassword" class="form-control" runat="server"></asp:TextBox>                    
+                    </div>
+                </div> 
+            </div>
+            <div class="row">       
+                <div class="col-md-12" > <!--Biografia-->
+                    <div class="form-group">
+                         <asp:Label ID="lblBiografia" runat="server" Text="Biografía:"></asp:Label>
+                         <asp:TextBox ID="txtBiografia" class="form-control" runat="server" TextMode="MultiLine"></asp:TextBox>                    
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</asp:Content>
---
+++ Escritores_Amateur_ASP/GUI/wfEditarUsuario.aspx.designer.cs
@@ -0,0 +1,231 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Escritores_Amateur_ASP.GUI {
+    
+    
+    public partial class wfEditarUsuario {
+        
+        /// <summary>
+        /// Control lblNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombre;
+        
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+        
+        /// <summary>
+        /// Control lblAvatar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAvatar;
+        
+        /// <summary>
+        /// Control txtAvatar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAvatar;
+        
+        /// <summary>
+        /// Control lblApellidoPat.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblApellidoPat;
+        
+        /// <summary>
+        /// Control txtApellidoPat.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellidoPat;
+        
+        /// <summary>
+        /// Control lblApellidoMat.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblApellidoMat;
+        
+        /// <summary>
+        /// Control txtApellidoMat.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellidoMat;
+        
+        /// <summary>
+        /// Control lblCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCorreo;
+        
+        /// <summary>
+        /// Control txtCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCorreo;
+        
+        /// <summary>
+        /// Control lblTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTelefono;
+        
+        /// <summary>
+        /// Control txtTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelefono;
+        
+        /// <summary>
+        /// Control lblMunicipio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMunicipio;
+        
+        /// <summary>
+        /// Control txtMunicipio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMunicipio;
+        
+        /// <summary>
+        /// Control lblSitioWeb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSitioWeb;
+        
+        /// <summary>
+        /// Control txtSitioWeb.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSitioWeb;
+        
+        /// <summary>
+        /// Control lblUsername.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsername;
+        
+        /// <summary>
+        /// Control txtUsername.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsername;
+        
+        /// <summary>
+        /// Control lblTipoUser.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTipoUser;
+        
+        /// <summary>
+        /// Control drpTipoUser.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList drpTipoUser;
+        
+        /// <summary>
+        /// Control lblPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPassword;
+        
+        /// <summary>
+        /// Control txtPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+        
+        /// <summary>
+        /// Control lblBiografia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblBiografia;
+        
+        /// <summary>
+        /// Control txtBiografia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtBiografia;
+    }
+}

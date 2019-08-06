8881a282c149afe00242ab1e6e097433c6eeceeb
Alan Moreno <alanalexismartinez@gmail.com>
2019-08-06 13:19:51 -0500

    Eventos y metodos para crear, eliminar y actualizar sinopsis

--- Escritores_Amateur_ASP/GUI/wfEditarSinopsis.aspx
+++ Escritores_Amateur_ASP/GUI/wfEditarSinopsis.aspx
@@ -6,18 +6,22 @@
         <div class="card-header">
             <ul class="nav nav-pills card-header-pills">
                 <div class="btn-group pull-right espacio">
-                    <asp:LinkButton ID="lbtnAgregar" runat="server" class="btn btn-primary">
+                    <asp:LinkButton ID="lbtnRegresar" runat="server" class="btn btn-primary" OnClick="lbtnRegresar_Click">
+                        Regresar </asp:LinkButton>
+                </div>
+                <div class="btn-group pull-right espacio">
+                    <asp:LinkButton ID="lbtnAgregar" runat="server" class="btn btn-primary" OnClick="lbtnAgregar_Click">
                         Agregar </asp:LinkButton>
                 </div>
                 &nbsp
                 <div class="btn-group pull-right espacio">
-                    <asp:LinkButton ID="lbtnEliminar" runat="server" class="btn btn-primary">
+                    <asp:LinkButton ID="lbtnEliminar" runat="server" class="btn btn-primary" OnClick="lbtnEliminar_Click">
                         Eliminar </asp:LinkButton>
                 </div>
 
                 &nbsp 
                 <div class="btn-group pull-right espacio">
-                    <asp:LinkButton ID="lbtnModificar" runat="server" class="btn btn-primary">
+                    <asp:LinkButton ID="lbtnModificar" runat="server" class="btn btn-primary" OnClick="lbtnModificar_Click">
                         Modificar</asp:LinkButton>
                 </div>
             </ul>
@@ -33,7 +37,7 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <!--Contenido-->
                     <div class="form-group">
                         <asp:Label ID="lblNombre" runat="server" Text="Contenido:"></asp:Label>
--- Escritores_Amateur_ASP/GUI/wfEditarSinopsis.aspx.cs
+++ Escritores_Amateur_ASP/GUI/wfEditarSinopsis.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 namespace Escritores_Amateur_ASP.GUI
 {
@@ -11,7 +12,147 @@ namespace Escritores_Amateur_ASP.GUI
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            cargaOperacion();
+        }
+        public void limpiar()
+        {
+            txtId.Text = "";
+            txtNombre.Text = "";
+        }
+        public void cargaOperacion()
+        {
+            lblId.Visible = false;
+            txtId.Visible = false;
+            if ((String)Session["frmSinopsisOperacion"] == "Editar")
+            {
+                limpiar();
+                buscar();
+                lbtnAgregar.Visible = false;
+            }
+            else
+            {
+                limpiar();
+                lbtnEliminar.Visible = false;
+                lbtnModificar.Visible = false;
+            }
+        }
+        public void buscar()
+        {
+            BO.Sinopsis obj = (BO.Sinopsis)Session["frmSinopsisBO"];
+            Servicios.SinopsisCtrl objCtrl = new Servicios.SinopsisCtrl();
+            DataTable dt = objCtrl.devuelveObj(obj);
+            if (dt.Rows.Count != 0)
+            {
+                txtId.Text = dt.Rows[0]["id_sinopsis"].ToString();
+                txtNombre.Text = dt.Rows[0]["contenido"].ToString();
+            }
+        }
+        public void agregar()
+        {
+            string mensaje = "";
+            if (txtNombre.Text.Trim().Length == 0)
+            {
+                mensaje = mensaje + "Introduce el contenido\n";
+            }
 
+            if (mensaje.Trim().Length == 0)
+            {
+                BO.Sinopsis obj = new BO.Sinopsis();
+                Servicios.SinopsisCtrl objCtrl = new Servicios.SinopsisCtrl();
+                obj.Contenido = txtNombre.Text.Trim();
+                string msn = objCtrl.creaSinopsis(obj);
+                if (msn == "La operación se realizó de manera correcta")
+                {
+                    Response.Redirect("wfTablaSinopsis.aspx");
+
+                }
+                else
+                {
+                    Mensaje(msn);
+                }
+            }
+            else
+            {
+                Mensaje("Favor de ingresar los siguientes datos:\n" + mensaje);
+            }
+        }
+        public void modificar()
+        {
+            string mensaje = "";
+            if (txtId.Text.Trim().Length == 0)
+            {
+                mensaje = mensaje + "Introduce la Clave \n";
+            }
+            if (txtNombre.Text.Trim().Length == 0)
+            {
+                mensaje = mensaje + "Introduce el contenido\n";
+            }
+
+            if (mensaje.Trim().Length == 0)
+            {
+                BO.Sinopsis obj = new BO.Sinopsis();
+                Servicios.SinopsisCtrl objCtrl = new Servicios.SinopsisCtrl();
+                obj.Id_sinopsis = Convert.ToInt32(txtId.Text.Trim().ToUpper());
+                obj.Contenido = txtNombre.Text.Trim().ToUpper();
+                string msn = objCtrl.actualizaObj(obj);
+                if (msn == "La operación se realizó de manera correcta")
+                {
+                    Response.Redirect("wfTablaSinopsis.aspx");
+
+                }
+                else
+                {
+                    Mensaje(msn);
+                }
+            }
+            else
+            {
+                Mensaje("Favor de ingresar los siguientes datos:\n" + mensaje);
+            }
+        }
+        public void eliminar()
+        {
+            BO.Sinopsis obj = (BO.Sinopsis)Session["frmSinopsisBO"];
+            Servicios.SinopsisCtrl objCtrl = new Servicios.SinopsisCtrl();
+            obj.Id_sinopsis = Convert.ToInt32(txtId.Text.Trim());
+            string mensaje = objCtrl.eliminaObj(obj);
+            if (mensaje == "La operación se realizó de manera correcta")
+            {
+                Response.Redirect("wfTablaCategoria.aspx");
+
+            }
+            else
+            {
+                Mensaje(mensaje);
+            }
+        }
+        private void Mensaje(string ex)
+        {
+            string mensaje = ex;
+            mensaje = mensaje.Replace(Environment.NewLine, "\\n");
+            mensaje = mensaje.Replace("\n", "\\n");
+            mensaje = mensaje.Replace("'", "\"");
+            ClientScript.RegisterClientScriptBlock(typeof(Page), "Error", "<script> alert('" + mensaje + "');</script>");
+        }
+
+        protected void lbtnRegresar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("wfTablaSinopsis.aspx");
+        }
+
+        protected void lbtnAgregar_Click(object sender, EventArgs e)
+        {
+            agregar();
+        }
+
+        protected void lbtnEliminar_Click(object sender, EventArgs e)
+        {
+            eliminar();
+        }
+
+        protected void lbtnModificar_Click(object sender, EventArgs e)
+        {
+            modificar();
         }
     }
 }

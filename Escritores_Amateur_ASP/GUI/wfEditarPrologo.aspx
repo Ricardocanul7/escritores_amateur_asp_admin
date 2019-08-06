16c0f8a82177ca4f2d50d66074b954e5dc5c1f05
Alan Moreno <alanalexismartinez@gmail.com>
2019-08-06 18:28:16 -0500

    Form para editar prologo, con métodos para cargar información. Eliminar, agregar, modificar prologo con sus eventos

---
+++ Escritores_Amateur_ASP/GUI/wfEditarPrologo.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage/mpAdmin.Master" AutoEventWireup="true" CodeBehind="wfEditarPrologo.aspx.cs" Inherits="Escritores_Amateur_ASP.GUI.wfEditarPrologo" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="cphTitulo" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphContenido" runat="server">
+    <div class="card text-center">
+        <div class="card-header">
+            <ul class="nav nav-pills card-header-pills">
+                <div class="btn-group pull-right espacio">
+                    <asp:LinkButton ID="lbtnRegresar" runat="server" class="btn btn-primary" OnClick="lbtnRegresar_Click">
+                        Regresar </asp:LinkButton>
+                </div>
+                <div class="btn-group pull-right espacio">
+                    <asp:LinkButton ID="lbtnAgregar" runat="server" class="btn btn-primary" OnClick="lbtnAgregar_Click">
+                        Agregar </asp:LinkButton>
+                </div>
+                &nbsp
+                <div class="btn-group pull-right espacio">
+                    <asp:LinkButton ID="lbtnEliminar" runat="server" class="btn btn-primary" OnClick="lbtnEliminar_Click" >
+                        Eliminar </asp:LinkButton>
+                </div>
+
+                &nbsp 
+                <div class="btn-group pull-right espacio">
+                    <asp:LinkButton ID="lbtnModificar" runat="server" class="btn btn-primary" OnClick="lbtnModificar_Click">
+                        Modificar</asp:LinkButton>
+                </div>
+            </ul>
+        </div>
+        <!-- Body -->
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <asp:Label ID="lblId" runat="server" Text="Id:"></asp:Label>
+                        <asp:TextBox ID="txtId" class="form-control" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <!--Contenido-->
+                    <div class="form-group">
+                        <asp:Label ID="lblNombre" runat="server" Text="Contenido:"></asp:Label>
+                        <asp:TextBox ID="txtNombre" class="form-control" textMode="MultiLine" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ Escritores_Amateur_ASP/GUI/wfEditarPrologo.aspx.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Escritores_Amateur_ASP.GUI
+{
+    public partial class wfEditarPrologo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cargaOperacion();
+        }
+        public void limpiar()
+        {
+            txtId.Text = "";
+            txtNombre.Text = "";
+        }
+        public void cargaOperacion()
+        {
+            lblId.Visible = false;
+            txtId.Visible = false;
+            if ((String)Session["frmPrologoOperacion"] == "Editar")
+            {
+                limpiar();
+                buscar();
+                lbtnAgregar.Visible = false;
+            }
+            else
+            {
+                limpiar();
+                lbtnEliminar.Visible = false;
+                lbtnModificar.Visible = false;
+            }
+        }
+        public void buscar()
+        {
+            BO.Prologo obj = (BO.Prologo)Session["frmPrologoBO"];
+            Servicios.PrologoCtrl objCtrl = new Servicios.PrologoCtrl();
+            DataTable dt = objCtrl.devuelveObj(obj);
+            if (dt.Rows.Count != 0)
+            {
+                txtId.Text = dt.Rows[0]["id_prologo"].ToString();
+                txtNombre.Text = dt.Rows[0]["contenido"].ToString();
+            }
+        }
+        public void agregar()
+        {
+            string mensaje = "";
+            if (txtNombre.Text.Trim().Length == 0)
+            {
+                mensaje = mensaje + "Introduce el contenido\n";
+            }
+
+            if (mensaje.Trim().Length == 0)
+            {
+                BO.Prologo obj = new BO.Prologo();
+                Servicios.PrologoCtrl objCtrl = new Servicios.PrologoCtrl();
+                obj.Contenido = txtNombre.Text.Trim();
+                string msn = objCtrl.creaPrologo(obj);
+                if (msn == "La operación se realizó de manera correcta")
+                {
+                    Response.Redirect("wfTablaPrologo.aspx");
+
+                }
+                else
+                {
+                    Mensaje(msn);
+                }
+            }
+            else
+            {
+                Mensaje("Favor de ingresar los siguientes datos:\n" + mensaje);
+            }
+        }
+        public void modificar()
+        {
+            string mensaje = "";
+            if (txtId.Text.Trim().Length == 0)
+            {
+                mensaje = mensaje + "Introduce la Clave \n";
+            }
+            if (txtNombre.Text.Trim().Length == 0)
+            {
+                mensaje = mensaje + "Introduce el contenido\n";
+            }
+
+            if (mensaje.Trim().Length == 0)
+            {
+                BO.Prologo obj = new BO.Prologo();
+                Servicios.PrologoCtrl objCtrl = new Servicios.PrologoCtrl();
+                obj.Id_prologo = Convert.ToInt32(txtId.Text.Trim().ToUpper());
+                obj.Contenido = txtNombre.Text.Trim().ToUpper();
+                string msn = objCtrl.actualizaObj(obj);
+                if (msn == "La operación se realizó de manera correcta")
+                {
+                    Response.Redirect("wfTablaPrologo.aspx");
+
+                }
+                else
+                {
+                    Mensaje(msn);
+                }
+            }
+            else
+            {
+                Mensaje("Favor de ingresar los siguientes datos:\n" + mensaje);
+            }
+        }
+        public void eliminar()
+        {
+            BO.Prologo obj = (BO.Prologo)Session["frmPrologoBO"];
+            Servicios.PrologoCtrl objCtrl = new Servicios.PrologoCtrl();
+            obj.Id_prologo = Convert.ToInt32(txtId.Text.Trim());
+            string mensaje = objCtrl.eliminaObj(obj);
+            if (mensaje == "La operación se realizó de manera correcta")
+            {
+                Response.Redirect("wfTablaPrologo.aspx");
+
+            }
+            else
+            {
+                Mensaje(mensaje);
+            }
+        }
+        private void Mensaje(string ex)
+        {
+            string mensaje = ex;
+            mensaje = mensaje.Replace(Environment.NewLine, "\\n");
+            mensaje = mensaje.Replace("\n", "\\n");
+            mensaje = mensaje.Replace("'", "\"");
+            ClientScript.RegisterClientScriptBlock(typeof(Page), "Error", "<script> alert('" + mensaje + "');</script>");
+        }
+
+        protected void lbtnRegresar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("wfTablaPrologo.aspx");
+        }
+
+        protected void lbtnAgregar_Click(object sender, EventArgs e)
+        {
+            agregar();
+        }
+
+        protected void lbtnEliminar_Click(object sender, EventArgs e)
+        {
+            eliminar();
+        }
+
+        protected void lbtnModificar_Click(object sender, EventArgs e)
+        {
+            modificar();
+        }
+    }
+}

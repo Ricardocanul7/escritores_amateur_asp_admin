6522360b2d2b02943b1ac7ddb10426cb1470a1e4
Ricardo Canul <ricardocanul7@gmail.com>
2019-08-06 22:29:47 -0500

    Se hacen cambios minimos

--- Escritores_Amateur_ASP/DAO/Categoria.cs
+++ Escritores_Amateur_ASP/DAO/Categoria.cs
@@ -45,38 +45,11 @@ namespace Escritores_Amateur_ASP.DAO
         {
             BO.Categoria data = (BO.Categoria)obj;
             bd = new BaseDB();
-            bd.Cmd.CommandType = CommandType.StoredProcedure;
 
-            sql = "INSERT INTO categoria(nombre_cat) VALUES(@nombre_cat)";
-            //sql = "EXEC SP_REGISTRO_USUARIO @nombre, @apellido_pat, @apellido_mat, @correo, @avatar, @municipio, @telefono, @sitio_web, @biografia, @username, @contrasenia, @tipo_usuario;";
-
-            //bd.Cmd.Parameters.Add("@nombre", SqlDbType.VarChar);
-            //bd.Cmd.Parameters.Add("@apellido_pat", SqlDbType.VarChar);
-            //bd.Cmd.Parameters.Add("@apellido_mat", SqlDbType.VarChar);
-            //bd.Cmd.Parameters.Add("@correo", SqlDbType.VarChar);
-            //bd.Cmd.Parameters.Add("@avatar", SqlDbType.VarChar);
-            //bd.Cmd.Parameters.Add("@municipio", SqlDbType.Int);
-            //bd.Cmd.Parameters.Add("@telefono", SqlDbType.Char);
-            //bd.Cmd.Parameters.Add("@sitio_web", SqlDbType.VarChar);
-            //bd.Cmd.Parameters.Add("@biografia", SqlDbType.Text);
-            //bd.Cmd.Parameters.Add("@username", SqlDbType.VarChar);
-            //bd.Cmd.Parameters.Add("@contrasenia", SqlDbType.VarChar);
-            //bd.Cmd.Parameters.Add("@tipo_usuario", SqlDbType.Int);
-
-            //bd.Cmd.Parameters["@nombre"].Value = data.Nombre;
-            //bd.Cmd.Parameters["@apellido_pat"].Value = data.Apellido_pat;
-            //bd.Cmd.Parameters["@apellido_mat"].Value = data.Apellido_mat;
-            //bd.Cmd.Parameters["@correo"].Value = data.Correo;
-            //bd.Cmd.Parameters["@avatar"].Value = data.Avatar;
-            //bd.Cmd.Parameters["@municipio"].Value = data.Municipio;
-            //bd.Cmd.Parameters["@telefono"].Value = data.Telefono;
-            //bd.Cmd.Parameters["@sitio_web"].Value = data.Sitio_web;
-            //bd.Cmd.Parameters["@biografia"].Value = data.Biografia;
-            //bd.Cmd.Parameters["@username"].Value = data.Username;
-            //bd.Cmd.Parameters["@contrasenia"].Value = data.Contrasenia;
-            //bd.Cmd.Parameters["@tipo_usuario"].Value = data.Tipo_usuario;
+            sql = "INSERT INTO categoria VALUES(@nombre_cat)";
 
             bd.Cmd.Parameters.AddWithValue("@nombre_cat", data.Nombre_cat);
+
             int i = bd.execNonQuery(sql);
             if (i == 0)
                 return 0;
@@ -88,10 +61,11 @@ namespace Escritores_Amateur_ASP.DAO
         {
             BO.Categoria data = (BO.Categoria)obj;
             bd = new BaseDB();
-            bd.Cmd.Parameters.Add("@id_categoria", SqlDbType.Int);
-            bd.Cmd.Parameters["@id_categoria"].Value = data.Id_categoria;
 
             sql = "DELETE FROM categoria WHERE id_categoria=@id_categoria";
+
+            bd.Cmd.Parameters.AddWithValue("@id_categoria", data.Id_categoria);
+
             int i = bd.execNonQuery(sql);
             if (i == 0)
             {
@@ -106,11 +80,9 @@ namespace Escritores_Amateur_ASP.DAO
             sql = "UPDATE categoria " +
                   "SET nombre_cat=@nombre_cat," +
                   " WHERE id_categoria=@id_categoria";
-            bd.Cmd.Parameters.Add("@id_categoria", SqlDbType.Int);
-            bd.Cmd.Parameters.Add("@nombre_cat", SqlDbType.VarChar);
 
-            bd.Cmd.Parameters["@id_categoria"].Value = data.Id_categoria;
-            bd.Cmd.Parameters["@nombre_cat"].Value = data.Nombre_cat;
+            bd.Cmd.Parameters.AddWithValue("@id_categoria", data.Id_categoria);
+            bd.Cmd.Parameters.AddWithValue("@nombre_cat", data.Nombre_cat);
 
             int i = bd.execNonQuery(sql);
             if (i == 0)
